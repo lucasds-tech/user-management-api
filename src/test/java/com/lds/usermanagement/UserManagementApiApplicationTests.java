70b5665c232ae15e64f04312d8a65fe27b560d75
lucasds-tech <lucas.dasilva09@hotmail.com>
2025-10-29 21:34:55 -0300

    initializr project Spring Boot v3.5.7 - user-management-api

---
+++ src/test/java/com/lds/usermanagement/UserManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lds.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
