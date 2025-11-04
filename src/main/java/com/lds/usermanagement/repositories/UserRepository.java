c9ff08ddcd4bcbb2bff610638b2e55b7e2a24240
lucasds-tech <lucas.dasilva09@hotmail.com>
2025-11-03 23:16:15 -0300

    feat: creating repositories

---
+++ src/main/java/com/lds/usermanagement/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lds.usermanagement.repositories;
+
+import com.lds.usermanagement.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
