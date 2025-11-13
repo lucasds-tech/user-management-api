aae33b9d84fce9cf36ae6340a6a8721920e0724b
lucasds-tech <lucas.dasilva09@hotmail.com>
2025-11-12 23:08:11 -0300

    refactor: UsersController to use UsersService for user operations

--- src/main/java/com/lds/usermanagement/controller/UsersController.java
+++ src/main/java/com/lds/usermanagement/controller/UsersController.java
@@ -3,71 +3,45 @@ package com.lds.usermanagement.controller;
 import com.devertelo.springswaggercodegen3.api.UsersApi;
 import com.devertelo.springswaggercodegen3.model.UserRequest;
 import com.devertelo.springswaggercodegen3.model.UserResponse;
+import com.lds.usermanagement.service.UsersService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/users")
 public class UsersController implements UsersApi {
 
-    private Map<String, UserResponse> users = new HashMap<>();
+    private final UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
 
     @Override
     public ResponseEntity<UserResponse> createUser(UserRequest userRequest) {
-        var user = toUserResponse(userRequest);
-        users.put(user.getId(), user);
-        return ResponseEntity.ok(user);
+        var users = usersService.createUser(userRequest);
+        return ResponseEntity.ok(users);
     }
 
     @Override
-    public ResponseEntity<Void> deleteUser(String id) {
-        UserResponse user = users.remove(id);
-        return user != null ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+    public ResponseEntity<Void> deleteUser(UUID id) {
+        boolean deletedUser = usersService.deleteUser(id);
+        return deletedUser ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
     @Override
-    public ResponseEntity<List<UserResponse>> getAllUsers() {
-        List<UserResponse> allUsers = this.users.values()
-                .stream()
-                .toList();
-        return ResponseEntity.ok(allUsers);
-    }
-
-    @Override
-    public ResponseEntity<UserResponse> getUserById(String id) {
-        var user = users.get(id);
+    public ResponseEntity<UserResponse> getUserById(UUID id) {
+        var user = usersService.getById(id);
         return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
     @Override
-    public ResponseEntity<UserResponse> updateUser(String id, UserRequest userRequest) {
-        var userById = users.get(id);
-
-        if (userById != null) {
-            var newUser = toUserResponse(userRequest);
-            newUser.setId(userById.getId());
-            newUser.setCreatedAt(userById.getCreatedAt());
-            users.put(id, newUser);
-            return ResponseEntity.ok(newUser);
-        }
-
-        return ResponseEntity.notFound().build();
-    }
-
-    private UserResponse toUserResponse(UserRequest userRequest) {
-        return new UserResponse()
-                .id(UUID.randomUUID().toString())
-                .username(userRequest.getUsername())
-                .name(userRequest.getName())
-                .lastName(userRequest.getLastName())
-                .email(userRequest.getEmail())
-                .password(userRequest.getPassword())
-                .status(userRequest.getStatus())
-                .createdAt(OffsetDateTime.now());
+    public ResponseEntity<UserResponse> updateUser(UUID id, UserRequest userRequest) {
+        var updated = usersService.updateUser(id, userRequest);
+        return updated != null ? ResponseEntity.ok(updated) : ResponseEntity.notFound().build();
     }
 }
