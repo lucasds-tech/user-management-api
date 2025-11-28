389e018b7d2c97d2951336e1f888d6790af982bb
lucasds-tech <lucas.dasilva09@hotmail.com>
2025-11-27 23:27:48 -0300

    feat: add methods to AddressRepository and UsersRepository for user-specific queries

--- src/main/java/com/lds/usermanagement/repositories/AddressRepository.java
+++ src/main/java/com/lds/usermanagement/repositories/AddressRepository.java
@@ -4,9 +4,10 @@ import com.lds.usermanagement.entity.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AddressRepository extends JpaRepository<AddressEntity, UUID> {
-
+    List<AddressEntity> findByUserId(UUID userId);
 }
--- src/main/java/com/lds/usermanagement/repositories/UsersRepository.java
+++ src/main/java/com/lds/usermanagement/repositories/UsersRepository.java
@@ -4,9 +4,11 @@ import com.lds.usermanagement.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UsersRepository extends JpaRepository<UsersEntity, UUID> {
-
+    Optional<UsersEntity> findByEmail(String email);
+    Optional<UsersEntity> findByUsername(String username);
 }
