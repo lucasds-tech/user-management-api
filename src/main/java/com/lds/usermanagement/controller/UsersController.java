73a5a7240d95f3ed6312941f40d0c13e332f5ba7
lucasds-tech <lucas.dasilva09@hotmail.com>
2026-02-09 22:56:16 -0300

    feat: implement UsersService for user management and update UsersController to handle String IDs

--- src/main/java/com/lds/usermanagement/controller/UsersController.java
+++ src/main/java/com/lds/usermanagement/controller/UsersController.java
@@ -5,14 +5,11 @@ import com.devertelo.springswaggercodegen3.model.UserRequest;
 import com.devertelo.springswaggercodegen3.model.UserResponse;
 import com.lds.usermanagement.service.UsersService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/users")
 public class UsersController implements UsersApi {
 
     private final UsersService usersService;
@@ -28,20 +25,20 @@ public class UsersController implements UsersApi {
     }
 
     @Override
-    public ResponseEntity<Void> deleteUser(UUID id) {
-        boolean deletedUser = usersService.deleteUser(id);
+    public ResponseEntity<Void> deleteUser(String id) {
+        boolean deletedUser = usersService.deleteUser(UUID.fromString(id));
         return deletedUser ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
     @Override
-    public ResponseEntity<UserResponse> getUserById(UUID id) {
-        var user = usersService.getById(id);
+    public ResponseEntity<UserResponse> getUserById(String id) {
+        var user = usersService.getById(UUID.fromString(id));
         return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
     @Override
-    public ResponseEntity<UserResponse> updateUser(UUID id, UserRequest userRequest) {
-        var updated = usersService.updateUser(id, userRequest);
+    public ResponseEntity<UserResponse> updateUser(String id, UserRequest userRequest) {
+        var updated = usersService.updateUser(UUID.fromString(id), userRequest);
         return updated != null ? ResponseEntity.ok(updated) : ResponseEntity.notFound().build();
     }
 }
